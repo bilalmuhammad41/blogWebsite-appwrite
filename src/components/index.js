a60c7594d4e8051a178ad3fde850b49b6ad892f9
bilalmuhammad41 <bilalmohammad41@gmail.com>
2024-02-09 19:21:18 +0500

    Login page and functionality done.

--- src/components/index.js
+++ src/components/index.js
@@ -2,9 +2,14 @@ import Footer from './Footer/Footer'
 import Header from './Header/Header'
 import Container from './Container/Container'
 import Logo from './Logo'
+import Input from './Input'
+import Button from './Button'
+
 export {
   Footer,
   Header,
   Container,
   Logo,
+  Button,
+  Input,
 }
